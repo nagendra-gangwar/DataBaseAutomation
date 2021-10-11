0be838cc7410877026b2e280e682b8d1406d949c
Nagendra Gangwar <nagendra.gang@gmail.com>
2021-10-11 12:01:16 +0800

    initial database check in

---
+++ DemoDatabase/Constraints/EmployeeDepartment.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[Employee]
+	ADD CONSTRAINT [EmployeeDepartment]
+	FOREIGN KEY (DepartmentID)
+	REFERENCES [Department] (Id)
---
+++ DemoDatabase/Tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Phone] NCHAR(15) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [DepartmentID] INT NOT NULL
+)
